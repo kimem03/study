7333d35a452f521dd03f740f668a505bd4d74952
kimem03 <112689621+kimem03@users.noreply.github.com>
2024-02-19 16:58:04 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,25 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq == "<":
+        if eq == "=":
+            if (n <= m) == 1:
+                answer = 1
+            else:
+                answer = 0
+        elif eq == "!":
+            if (n < m) == 1:
+                answer = 1
+            else:
+                answer = 0
+    elif ineq == ">":
+        if eq == "=":
+            if (n >= m) == 1:
+                answer = 1
+            else:
+                answer = 0
+        elif eq == "!":
+            if (n > m) == 1:
+                answer = 1
+            else:
+                answer = 0
+    return answer
