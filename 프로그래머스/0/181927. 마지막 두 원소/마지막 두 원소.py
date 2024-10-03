e2f2215b237168facea5248c7ba4ebe10b70bc79
kimem03 <112689621+kimem03@users.noreply.github.com>
2024-10-03 17:31:18 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,13 +1,12 @@
 def solution(num_list):
-    answer = num_list
     
     l = len(num_list) - 1
     z = num_list[l]
     y = num_list[l-1]
     
     if(z > y):
-        answer.append(z-y)
+        num_list.append(z-y)
     else:
-        answer.append(z*2)
+        num_list.append(z*2)
         
-    return answer
+    return num_list
